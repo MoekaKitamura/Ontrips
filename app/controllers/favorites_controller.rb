556c1c76ad53f9d80d63675b9a6f82aaab7ff3b9
moeka kitamura <moeka.k76@gmail.com>
2021-10-01 01:30:07 +0900

    fix #12 & font, logo

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(trip_id: params[:trip_id])
-    redirect_to trips_path, notice: "#{favorite.trip.user.name}さんの投稿をいいねしました"
+    redirect_to trip_path(params[:trip_id]), notice: "#{favorite.trip.user.name}さんの旅をお気に入りしました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to trips_path, notice: "#{favorite.trip.user.name}さんの投稿をいいね解除しました"
+    redirect_to trip_path(favorite.trip.id), notice: "#{favorite.trip.user.name}さんの旅のお気に入りを解除しました"
   end
 end
