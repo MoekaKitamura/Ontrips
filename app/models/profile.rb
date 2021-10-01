d19e08b49729bb14e84b0e31889750224f2749fe
moeka kitamura <moeka.k76@gmail.com>
2021-10-01 09:32:00 +0900

    set all enum #6

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,10 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   mount_uploader :icon, IconUploader
+
+  enum gender: { 男性: 1, 女性: 2, その他: 3 }
+  enum home_country: { 日本: 1, オーストラリア: 2, アメリカ: 3 }
+  enum home_city: { 福岡: 1, 東京: 2, 大阪: 3 }
+  enum first_language: { 日本語: 1, 英語: 2, スペイン語: 3 }, _suffix: true #多重定義防止trueの場合は、属性名と同じ
+  enum second_language: { 日本語: 1, 英語: 2, スペイン語: 3 }, _suffix: true
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,4 +8,7 @@ class Trip < ApplicationRecord
   has_many :member_users, through: :members, source: :user
 
   has_many :comments, dependent: :destroy
+
+  enum country: { 日本: 1, オーストラリア: 2, アメリカ: 3 }
+  enum city: { 福岡: 1, 東京: 2, 大阪: 3 }
 end
