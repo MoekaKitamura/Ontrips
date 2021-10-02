212b12b2525304b46852dd49ebd22c59e017ef11
moeka kitamura <moeka.k76@gmail.com>
2021-10-02 14:53:47 +0900

    validation for member finished trip#3

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,11 +1,17 @@
 class MembersController < ApplicationController
   def create
-    member = current_user.members.create(trip_id: params[:trip_id], as: params[:as])
-    if member.as == 1
-      redirect_to trip_path(params[:trip_id]), notice: "#{member.trip.user.name}さんの旅に旅人として参加しました！"
+    @trip = Trip.find(params[:trip_id])
+    unless @trip.goal
+      member = current_user.members.create(trip_id: params[:trip_id], as: params[:as])
+      if member.as == 1
+        redirect_to trip_path(params[:trip_id]), notice: "#{member.trip.user.name}さんの旅に旅人として参加しました！"
+      else
+        redirect_to trip_path(params[:trip_id]), notice: "#{member.trip.user.name}さんの旅に現地人として参加しました！"
+      end
     else
-      redirect_to trip_path(params[:trip_id]), notice: "#{member.trip.user.name}さんの旅に現地人として参加しました！"
+      redirect_to @trip, notice: 'この旅行は終了しています。終了した旅行に参加できません。'
     end
+    
   end
 
   def destroy
