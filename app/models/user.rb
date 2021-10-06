a2a02aa2c37b659c7f1c0883cf9731de6fe803c9
moeka kitamura <moeka.k76@gmail.com>
2021-10-06 09:49:35 +0900

    guest log in #39

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   has_many :members, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  def self.guest
+    find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+      user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,15 @@ Rails.application.routes.draw do
   end
   resources :blogs
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
