8f0efa1f37277088c7f87dba4417bca8d52bf583
moeka kitamura <moeka.k76@gmail.com>
2021-10-06 17:21:22 +0900

    build profile when creating user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
+
+  def create
+    super
+    @profile = @user.build_profile(id: @user.id)
+    @profile.save
+  end
 end
