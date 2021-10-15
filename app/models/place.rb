13287245a29461394bce108baeac275e0c0b7593
moeka kitamura <moeka.k76@gmail.com>
2021-10-15 12:06:18 +0900

    modified Trip table column & association

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,4 @@
 class Place < ApplicationRecord
   has_ancestry
+  has_many :trips
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_015713) do
+ActiveRecord::Schema.define(version: 2021_10_15_025925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,8 +100,6 @@ ActiveRecord::Schema.define(version: 2021_10_15_015713) do
 
   create_table "trips", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "country", null: false
-    t.integer "city"
     t.date "start_on"
     t.date "finish_on"
     t.boolean "flexible", default: false
@@ -110,6 +108,8 @@ ActiveRecord::Schema.define(version: 2021_10_15_015713) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "place_id"
+    t.index ["place_id"], name: "index_trips_on_place_id"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
@@ -147,5 +147,6 @@ ActiveRecord::Schema.define(version: 2021_10_15_015713) do
   add_foreign_key "messages", "talks"
   add_foreign_key "messages", "users"
   add_foreign_key "profiles", "users"
+  add_foreign_key "trips", "places"
   add_foreign_key "trips", "users"
 end
