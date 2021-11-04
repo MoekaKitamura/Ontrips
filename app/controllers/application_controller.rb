3805567f8198f7f79a0d4a6b6b85559fd33bb668
moeka kitamura <moeka.k76@gmail.com>
2021-11-04 20:20:34 +0900

    change after signin path #85

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    profile_path(resource_or_scope)
+    profile_path(resource_or_scope.profile.id)
   end
 
 end
